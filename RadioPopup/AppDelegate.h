6f4cab817831bbe3ceca53fdd669c2cdc9052312
bugkingK <myway0710@naver.com>
2019-05-15 14:55:41 +0900

    Initial Commit

---
+++ RadioPopup/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RadioPopup
+//
+//  Created by moon on 15/05/2019.
+//  Copyright © 2019 bugking. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
